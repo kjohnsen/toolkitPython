edc54f1026f619fc45142e8b509b37487adec665
Kyle Johnsen <johnsenkyle13@gmail.com>
2019-02-15 11:38:34 -0700

    Handle multiple classes

--- learners/perceptron.py
+++ learners/perceptron.py
@@ -4,7 +4,7 @@ import numpy as np
 class Perceptron:
     def __init__(self, num_features, learning_rate):
         # last weight is bias
-        self.weights = np.array([random.uniform(0.1, 0.5) for x in range(num_features + 1)])
+        self.weights = np.array([random.uniform(0.1, 0.5) for x in range(num_features)])
         self.l_rate = learning_rate
 
     # Returns 1 if it was correct, to keep track of accuracy
@@ -18,11 +18,14 @@ class Perceptron:
         else: return 0
         
 
-    def calculate_output(self, inputs):
+    def calculate_output(self, inputs, net=False):
         # print(f"Inputs = {inputs}")
         # print(f'Weights = {self.weights}')
         weighted_in = np.multiply(inputs, self.weights)
-        net = np.sum(weighted_in)
-        if net > 0: return 1
-        else: return 0
+        net_out = np.sum(weighted_in)
+        if net_out > 0: out = 1
+        else: out = 0
+
+        if net: return (out, net_out)
+        else: return out
 
--- learners/perceptron_learner.py
+++ learners/perceptron_learner.py
@@ -11,6 +11,7 @@ from toolkit.supervised_learner import SupervisedLearner
 from learners.perceptron import Perceptron
 
 class PerceptronLearner(SupervisedLearner):
+    lr = 0.1
     def __init__(self):
         pass
 
@@ -20,39 +21,81 @@ class PerceptronLearner(SupervisedLearner):
         :type features: Matrix
         :type labels: Matrix
         """
+        self.perceptrons = []
+
         for row in features.data:
             row.append(1)  # add bias input
+        features.attr_names.append("bias")
         
-        if labels.value_count(0) == 2:
-            self.percy = Perceptron(2, 0.1)
-            boring_epochs = 0
-            best_correct = 0
-            while boring_epochs < 10:
-                correct = 0
-                for i in range(features.rows):
-                    inputs = features.row(i)
-                    target = labels.row(i)[0]
-                    correct += self.percy.process_instance(inputs, target)
+        self.num_classes = labels.value_count(0)
+        if self.num_classes == 2:
+            percy = Perceptron(features.cols, self.lr)
+            self.perceptrons.append(percy)
+            self.train_perceptron(percy, features, labels)
+        elif self.num_classes > 2:
+            for class_enum in range(self.num_classes):
+                class_specific_labels = labels.copy()
+                # necessary so original features don't get shuffled separate from original labels
+                features_copy = features.copy()
+                for row in class_specific_labels.data:
+                    if row[0] == class_enum:
+                        row[0] = 1
+                    else:
+                        row[0] = 0
+                percy = Perceptron(features.cols, self.lr)
+                self.perceptrons.append(percy)
+                self.train_perceptron(percy, features_copy, class_specific_labels)
 
-                if correct > best_correct:
-                    boring_epochs = 0
-                    best_correct = correct
-                    print(f'Correct = {correct}')
-                else: 
-                    boring_epochs += 1
-                    # TODO: refactor to use measure_accuracy
-        
-        else:
-            raise Exception("Non-binary classification not implemented")
+    def train_perceptron(self, perceptron, features, labels):
+        boring_epochs = 0
+        epochs = 0
+        best_correct = 0
+        while boring_epochs < 10:
+            correct = 0
+            for i in range(features.rows):
+                inputs = features.row(i)
+                target = labels.row(i)[0]
+                correct += perceptron.process_instance(inputs, target)
+
+            if correct > best_correct:
+                boring_epochs = 0
+                best_correct = correct
+            else: 
+                boring_epochs += 1
+
+            epochs += 1
+            features.shuffle(labels)
+            print(f'Accuracy for epoch = {correct/features.rows}')
+            # misclassification rate
+            # print(1-self.measure_accuracy(features, labels))
+
+        # print(f'Final weight vector:')
+        # for i in range(len(perceptron.weights)-1):
+        #     feature = features.attr_name(i)
+        #     print(f'{feature}: {perceptron.weights[i]}')
+        print(f'{epochs} epochs elapsed in training')
+        # print(f'Accuracy for perceptron: {self.measure_accuracy(features, labels)}')
 
 
     def predict(self, features, labels):
-        """
-        :type features: [float]
-        :type labels: [float]
-        """
         del labels[:]
         inputs = features.copy()
-        if len(inputs) == len(self.percy.weights) - 1: inputs.append(1)
-        labels.append(self.percy.calculate_output(inputs))
+        if len(inputs) == len(self.perceptrons[0].weights) - 1: inputs.append(1)
+
+        if self.num_classes == 2:
+            labels.append(self.perceptrons[0].calculate_output(inputs))
+
+        elif self.num_classes > 2:
+            highest_net = -float('inf')
+            # should work for any number of classes
+            for class_enum in range(len(self.perceptrons)):
+                percy = self.perceptrons[class_enum]
+                out, net = percy.calculate_output(inputs, net=True)
+                # print(f'class: {class_enum}, net: {net}, out: {out}')
+                if net > highest_net:
+                    highest_net = net
+                    winner = class_enum
+            # print(f'highest net is {highest_net} for class {winner}')
+            labels.append(winner)
+
 
